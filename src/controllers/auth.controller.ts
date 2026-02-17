0ea320e8cc17301c754dbf738bcc55d9d643a685
althair-s <althair.assassint@gmail.com>
2026-02-17 08:26:51 +0800

    feat : fitur login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import * as yup from "yup";
 
 import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
 
 type TRegister = {
     fullname: string;
@@ -9,6 +10,11 @@ type TRegister = {
     email: string;
     password: string;
     confirmPassword: string;
+};
+
+type TLogin = {
+    identifier: string;
+    password: string;
 };
 
 const registerValidateSchema = yup.object({
@@ -42,12 +48,53 @@ export default {
 
             res.status(200).json({
                 message: "User registered successfully",
-                data : result,
+                data: result,
             });
         } catch (error) {
             const err = error as unknown as Error;
             res.status(400).json({
                 message: err.message,
+            });
+        }
+    },
+
+    async login(req: Request, res: Response) {
+        const { identifier, password } =
+            req.body as unknown as TLogin;
+
+        try {
+            //Ambil data user berdasarkan "identifier" -> email & username
+            const userByIdentifier = await UserModel.findOne({
+                $or: [
+                    { email: identifier, },
+                    { username: identifier, },
+                ],
+            });
+            if (!userByIdentifier) {
+                return res.status(403).json({
+                    message: "User not found",
+                    data: null,
+                });
+            }
+            // validasi password
+            const validatePassword : boolean = encrypt(password) === userByIdentifier.password;
+            if (!validatePassword) {
+                return res.status(403).json({
+                    message: "user not found",
+                    data: null,
+                });
+            }
+
+            res.status(200).json({
+                message: "User Login Success",
+                data: userByIdentifier,
+            });
+
+        } catch (error) {
+            const err = error as unknown as Error;
+            res.status(400).json({
+                message: err.message,
+                data: null,
             });
         }
     },
