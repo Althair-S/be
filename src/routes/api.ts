48bfd17eb2b0dd3c39ee43cc2e79db97efc11750
althair-s <althair.assassint@gmail.com>
2026-02-18 10:24:00 +0800

    feat : access control list

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 import authController from "../controllers/auth.controller";
 import authMiddleware from "../middlewares/auth.middleware";
-
+import aclMiddleware from "../middlewares/acl.middleware";
+import { ROLES } from "../utils/constant";
+// import userController from "../controllers/user.controller";
 
 const router = express.Router();
 
@@ -10,5 +12,13 @@ router.post("/auth/register", authController.register);
 router.post("/auth/login", authController.login);
 router.get("/auth/me", authMiddleware, authController.me);
 router.post("/auth/activation", authController.activation);
+
+router.get("/test-acl", [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
+  (req: Request, res: Response) => {
+    res.status(200).json({
+      data: "success",
+      message: "OK"
+    });
+  });
 
 export default router;
