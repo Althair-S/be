3adc668e435fdae0c8dfc4d37b67cff072b18971
althair-s <althair.assassint@gmail.com>
2026-02-19 15:27:09 +0800

    feat : add region ~ village dataset

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,6 +7,7 @@ import { ROLES } from "../utils/constant";
 import mediaMiddleware from "../middlewares/media.middleware";
 import mediaController from "../controllers/media.controller";
 import categoryController from "../controllers/category.controller";
+import regionController from "../controllers/region.controller";
 
 const router = express.Router();
 
@@ -20,6 +21,14 @@ router.get("/category", categoryController.findAll);
 router.get("/category/:id", categoryController.findOne);
 router.put("/category/:id", [authMiddleware, aclMiddleware([ROLES.ADMIN])], categoryController.update);
 router.delete("/category/:id", [authMiddleware, aclMiddleware([ROLES.ADMIN])], categoryController.remove);
+
+router.get("/regions", regionController.getAllProvinces);
+router.get("/regions/:id/province", regionController.getProvince);
+router.get("/regions/:id/regency", regionController.getRegency);
+router.get("/regions/:id/district", regionController.getDistrict);
+router.get("/regions/:id/village", regionController.getVillage);
+router.get("/regions-search", regionController.findByCity);
+
 
   router.post('/media/upload-single', [
     authMiddleware,
