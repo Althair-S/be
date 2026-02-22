31f0eeabc3857b0f93b20abb337c3374b52a1734
althair-s <althair.assassint@gmail.com>
2026-02-22 09:53:17 +0800

    feat : global error handler

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import response from "../utils/response";
+
+
+export default {
+  serverRoute() {
+    return (req: Request, res: Response, next: NextFunction) => {
+      response.notFound(res, "Route Not Found");
+    };
+  },
+
+  serverError() {
+    return (err: Error, req: Request, res: Response, next: NextFunction) => {
+      response.error(res, err, err.message);
+    };
+  },
+
+}
